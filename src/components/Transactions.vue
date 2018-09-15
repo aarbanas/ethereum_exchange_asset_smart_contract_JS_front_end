ad568ac41829f208c4dcc3c8f95b4a0ca2555ab3
Andrej Arbanas <aarbanas@riteh.hr>
2018-09-15 17:45:15 +0200

    Update Transactions.vue

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -7,6 +7,7 @@
   
       <v-data-table :headers="headers" :items="items" hide-actions class="elevation-1">
         <template slot="items" slot-scope="props">
+          <td class="text-xs-left">{{ props.item.date }}</td>
           <td class="text-xs-left">{{ props.item.to }}</td>
           <td class="text-xs-left">{{ props.item.from }}</td>
           <td class="text-xs-left">{{ props.item.amount }}</td>
@@ -22,6 +23,7 @@ export default {
   data () {
     return {
       headers: [
+        { align: 'left', text: 'Date', value: 'date' },
         { align: 'left', text: 'New', value: 'to' },
         { align: 'left', text: 'Old', value: 'from' },
         { align: 'left', text: 'Price', value: 'amount' }
@@ -34,6 +36,7 @@ export default {
       if (res.args.to === CONTRACT._eth.coinbase || res.args.from === CONTRACT._eth.coinbase) {
           console.log(res)
         this.items.push({
+          date: Date(res.args.saleDate * 1000),
           to: res.args.to,
           from: res.args.from,
           amount: web3.fromWei(res.args._salePrice).toNumber()
