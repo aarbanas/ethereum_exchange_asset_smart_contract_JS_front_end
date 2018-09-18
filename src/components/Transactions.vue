ce2f29295182fe3d3abeb582d1dc19331fc878cf
Andrej Arbanas <aarbanas@riteh.hr>
2018-09-18 20:07:20 +0200

    Update Transactions.vue

--- src/components/Transactions.vue
+++ src/components/Transactions.vue
@@ -2,15 +2,16 @@
   <v-flex xs12>
     <v-card class="card--flex-toolbar">
       <v-toolbar card class="light-blue">
-        <v-toolbar-title class="white--text">Owners</v-toolbar-title>
+        <v-toolbar-title class="white--text">Transfers</v-toolbar-title>
       </v-toolbar>
   
       <v-data-table :headers="headers" :items="items" hide-actions class="elevation-1">
         <template slot="items" slot-scope="props">
           <td class="text-xs-left">{{ props.item.date }}</td>
+          <td class="text-xs-center">{{ props.item.blockNumber }}</td>
           <td class="text-xs-left">{{ props.item.to }}</td>
           <td class="text-xs-left">{{ props.item.from }}</td>
-          <td class="text-xs-left">{{ props.item.amount }}</td>
+          <td class="text-xs-right">{{ props.item.amount }}</td>
         </template>
       </v-data-table>
   
@@ -24,6 +25,7 @@ export default {
     return {
       headers: [
         { align: 'left', text: 'Date', value: 'date' },
+        { align: 'left', text: 'Block No.', value: 'blockNumber' },
         { align: 'left', text: 'New', value: 'to' },
         { align: 'left', text: 'Old', value: 'from' },
         { align: 'left', text: 'Price', value: 'amount' }
@@ -37,6 +39,7 @@ export default {
           console.log(res)
         this.items.push({
           date: Date(res.args.saleDate * 1000),
+          blockNumber: res.args._blockNumber,
           to: res.args.to,
           from: res.args.from,
           amount: web3.fromWei(res.args._salePrice).toNumber()
